5f868e09e6c574707d5a5373e50aeac5d30a0b1b
Rafael Neves <rafaelneves3110@gmail.com>
2021-05-09 19:40:04 -0300

    Add files via upload

---
+++ Create_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `uniasselvi`.`lista` (
+  `Cod_Item` INT NOT NULL,
+  `Nome_Item` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Cod_Item`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
